e90c41f554bd7268a914cf35681856dbe00b927e
pr0fix <bgx152@myy.haaga-helia.fi>
2025-03-04 13:42:38 +0200

    implement desktop UI customers table loading skeleton

--- app/ui/skeletons.tsx
+++ app/ui/skeletons.tsx
@@ -242,6 +242,36 @@ export function CustomersMobileSkeleton() {
   );
 }
 
+export function CustomersTableRowSkeleton() {
+  return (
+    <tr className="w-full border-b border-gray-100 last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg">
+      {/* Customer Name and Image */}
+      <td className="relative overflow-hidden whitespace-nowrap py-3 pl-6 pr-3">
+        <div className="flex items-center gap-3">
+          <div className="h-8 w-8 rounded-full bg-gray-100"></div>
+          <div className="h-6 w-24 rounded bg-gray-100"></div>
+        </div>
+      </td>
+      {/* Email */}
+      <td className="whitespace-nowrap px-3 py-3">
+        <div className="h-6 w-32 rounded bg-gray-100"></div>
+      </td>
+      {/* Total Invoices Amount */}
+      <td className="whitespace-nowrap px-3 py-3">
+        <div className="h-6 w-16 rounded bg-gray-100"></div>
+      </td>
+      {/* Total Pending Amount */}
+      <td className="whitespace-nowrap px-3 py-3">
+        <div className="h-6 w-16 rounded bg-gray-100"></div>
+      </td>
+      {/* Total Paid Amount */}
+      <td className="whitespace-nowrap px-3 py-3">
+        <div className="h-6 w-16 rounded bg-gray-100"></div>
+      </td>
+    </tr>
+  );
+}
+
 export function CustomersTableSkeleton() {
   return (
     <div className="mt-6 flow-root">
@@ -276,12 +306,12 @@ export function CustomersTableSkeleton() {
               </tr>
             </thead>
             <tbody className="bg-white">
-              <InvoicesTableRowSkeleton />
-              <InvoicesTableRowSkeleton />
-              <InvoicesTableRowSkeleton />
-              <InvoicesTableRowSkeleton />
-              <InvoicesTableRowSkeleton />
-              <InvoicesTableRowSkeleton />
+              <CustomersTableRowSkeleton />
+              <CustomersTableRowSkeleton />
+              <CustomersTableRowSkeleton />
+              <CustomersTableRowSkeleton />
+              <CustomersTableRowSkeleton />
+              <CustomersTableRowSkeleton />
             </tbody>
           </table>
         </div>
