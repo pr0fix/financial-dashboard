cc6ede276cab3ef29f0aa9d3c0847ea593852c4a
pr0fix <bgx152@myy.haaga-helia.fi>
2025-03-05 20:02:04 +0200

    add development pipeline, custom eslint config & fix lint errors

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -2,7 +2,6 @@ import postgres from "postgres";
 import {
   CustomerField,
   CustomersTableType,
-  FormattedCustomersTable,
   InvoiceForm,
   InvoicesTable,
   LatestInvoiceRaw,
@@ -16,16 +15,7 @@ const sql = postgres(process.env.POSTGRES_URL!, {
 
 export async function fetchRevenue() {
   try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
-
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
-
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
-
-    // console.log('Data fetch completed after 3 seconds.');
-
     return data;
   } catch (error) {
     console.error("Database Error:", error);
@@ -55,9 +45,7 @@ export async function fetchLatestInvoices() {
 
 export async function fetchCardData() {
   try {
-    // You can probably combine these into a single SQL query
-    // However, we are intentionally splitting them to demonstrate
-    // how to initialize multiple queries in parallel with JS.
+    // TODO: combine these into a single SQL query
     const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
     const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
     const invoiceStatusPromise = sql`SELECT
