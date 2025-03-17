e9a84270c86a13f1a8c69aa4803f672e6da50d59
pr0fix <bgx152@myy.haaga-helia.fi>
2025-03-17 11:42:59 +0200

    add signup page unit tests

--- __tests__/signup.test.tsx
+++ __tests__/signup.test.tsx
@@ -1,46 +1,43 @@
 import "@testing-library/jest-dom";
 import { fireEvent, render, screen } from "@testing-library/react";
-import LoginPage from "../app/login/page";
+import SignUpPage from "../app/sign-up/page";
 
 jest.mock("../app/lib/actions", () => ({
   authenticate: jest.fn(() => Promise.resolve({ success: true })),
 }));
 
-jest.mock("next/navigation", () => ({
-  useSearchParams: () => ({
-    get: jest.fn((param) => {
-      if (param === "callbackUrl") return "/dashboard";
-      return null;
-    }),
-  }),
-}));
-
-describe("Login-page", () => {
+describe("Sign-up-page", () => {
   it("renders the instruction text", () => {
-    render(<LoginPage />);
+    render(<SignUpPage />);
 
-    expect(screen.getByText("Please log in to continue.")).toBeInTheDocument();
+    expect(screen.getByText("Create your account")).toBeInTheDocument();
   });
 
-  it("renders email and password input fields", () => {
-    render(<LoginPage />);
+  it("renders name, email and password input fields", () => {
+    render(<SignUpPage />);
+    expect(screen.getByLabelText(/name/i)).toBeInTheDocument();
     expect(screen.getByLabelText(/email/i)).toBeInTheDocument();
     expect(screen.getByLabelText(/password/i)).toBeInTheDocument();
   });
 
-  it("renders the login button", () => {
-    render(<LoginPage />);
-    expect(screen.getByRole("button", { name: /log in/i })).toBeInTheDocument();
+  it("renders the sign up button", () => {
+    render(<SignUpPage />);
+    expect(
+      screen.getByRole("button", { name: /sign up/i })
+    ).toBeInTheDocument();
   });
 
   it("allows user to enter credentials", () => {
-    render(<LoginPage />);
+    render(<SignUpPage />);
+    const nameInput = screen.getByLabelText(/name/i);
     const emailInput = screen.getByLabelText(/email/i);
     const passwordInput = screen.getByLabelText(/password/i);
 
+    fireEvent.change(nameInput, { target: { value: "user" } });
     fireEvent.change(emailInput, { target: { value: "user@example.com" } });
     fireEvent.change(passwordInput, { target: { value: "password123" } });
 
+    expect(nameInput).toHaveValue("user");
     expect(emailInput).toHaveValue("user@example.com");
     expect(passwordInput).toHaveValue("password123");
   });
